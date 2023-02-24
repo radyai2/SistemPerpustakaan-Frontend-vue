d1309c392e55fee0b3f75c1f8c72141cd3594c79
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-24 09:27:28 +0700

    remove HomeView import

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 import DashboardView from '../components/DashboardView.vue'
 import UserView from '../components/UserView.vue'
 import BukuView from '../components/BukuView.vue'
