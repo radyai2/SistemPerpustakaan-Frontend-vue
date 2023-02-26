8d30fc81ff31a4cde2d0550504130285bfaefdc9
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-26 16:39:37 +0700

    Add save edit method

--- src/components/crud_siswa/EditSiswa.vue
+++ src/components/crud_siswa/EditSiswa.vue
@@ -19,21 +19,21 @@
                                 <div class="card-body">
                                     <form>
                                         <label for="nama" class="form-label">Nama:</label>
-                                        <input type="text" v-model="formData.nama_siswa" class="form-control" id="nama" autocomplete="off" placeholder="Masukkan nama..">
+                                        <input type="text" class="form-control" v-model="siswa.nama_siswa" id="nama" autocomplete="off" placeholder="Masukkan nama..">
 
                                         <label for="tgl_lahir" class="form-label">Tanggal Lahir:</label>
-                                        <input type="date" class="form-control" id="tgl_lahir">
+                                        <input type="date" class="form-control" v-model="siswa.tanggal_lahir" id="tgl_lahir">
 
                                         <label for="gender" class="form-label">Gender:</label>
-                                        <select id="gender" class="form-control">
+                                        <select v-model="siswa.gender" id="gender" class="form-control">
                                         <option value="L">L</option>
                                         <option value="P">P</option>
                                         </select>
 
                                         <label for="alamat" class="form-labek">Alamat:</label>
-                                        <textarea id="alamat" class="form-control"></textarea>
+                                        <textarea v-model="siswa.alamat" id="alamat" class="form-control"></textarea>
                                         <br>
-                                        <input type="submit" class="btn btn-primary">
+                                        <input type="submit"  @click.prevent="save_edit" class="btn btn-primary">
 
                                     </form>
                                 </div>
@@ -55,22 +55,41 @@ import NavigationBar from '../template/NavigationBar.vue'
 import AppSidebar from '../template/AppSidebar.vue'
 
 export default {
+    props: ['id'],  
     components:{
         'navbar-component' : NavigationBar,
         'sidebar-component' : AppSidebar,
     },
-    data(){
+   data(){
         return{
-            formData:{}
+            siswa:{}
         }
-    },
-    created(){
-        this.DataSiswa();
-    },
-    methods: {
-        DataSiswa(){
-            axios.get('http://127.0.0.1:8000/api/getsiswa/')
-        }
-    }
+   },
+   mounted(){
+    this.GetSiswa()
+   },
+   methods: {
+        GetSiswa(){
+            axios.get('http://127.0.0.1:8000/api/getsiswa/' + this.$route.params.id)
+            .then(
+                ({data}) => {
+                    console.log(data);
+                    this.siswa = data
+                }
+            );
+        },
+        save_edit(){
+            axios.patch('http://127.0.0.1:8000/api/updatesiswa/' + this.$route.params.id , this.siswa)
+            .then(
+                ({data}) => {
+                    alert('Sukses update siswa');
+                    this.$router.push('/user');
+                    this.siswa = data;
+                }
+            );
+        },
+    }    
 }
+   
+
 </script>
